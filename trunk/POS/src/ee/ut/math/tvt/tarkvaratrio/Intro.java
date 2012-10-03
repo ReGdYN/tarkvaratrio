b7fdf9719f0387940d5495634bfc2d032a073b59
regdyn@gmail.com <regdyn@gmail.com@ab323cf8-aadf-9211-8cbf-a8e3bd1d72ed>
2012-10-03 10:00:32 +0000

    Just a commit test

---
+++ trunk/POS/src/ee/ut/math/tvt/tarkvaratrio/Intro.java
@@ -0,0 +1,13 @@
+package ee.ut.math.tvt.tarkvaratrio;
+
+public class Intro {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+	}
+
+}
