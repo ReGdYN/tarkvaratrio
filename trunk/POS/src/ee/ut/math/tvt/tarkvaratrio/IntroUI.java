d76383aac54d7ca2ddf31cc1677a38d0db6ecff2
regdyn@gmail.com <regdyn@gmail.com@ab323cf8-aadf-9211-8cbf-a8e3bd1d72ed>
2012-10-03 15:26:23 +0000

    Generated build file, w/ locations and image library, try it, needs a few gentle touches

--- trunk/POS/src/ee/ut/math/tvt/tarkvaratrio/IntroUI.java
+++ trunk/POS/src/ee/ut/math/tvt/tarkvaratrio/IntroUI.java
@@ -1,8 +1,12 @@
 package ee.ut.math.tvt.tarkvaratrio;
 
 import java.awt.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.swing.*; 
 
+@SuppressWarnings("unused")
 public class IntroUI {
 	
 	static String kursus = "2.kursus";
@@ -10,24 +14,43 @@ public class IntroUI {
 	static String juht = "Taavi Jaanson";
 	static String email = "regdyn@gmail.com";
 	static String liikmed = "Kristjan-Julius Laak, Henri Molloka";
-	
+	private final static String uusRida = "\n";
+	static URL asukoht;
 	
 	static void createWindow() {
 		JFrame raam = new JFrame("IntroUI");
 		raam.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container sisu = raam.getContentPane();
+		sisu.setLayout(new GridLayout(3,1));
+		JLabel label1;
+		JTextArea label2, label3;
 		
-		//String kursus = "2. kurus";
-		//raam.getContentPane().add(kursus);
-		//raam.add(new JTextField(kursus));
+		JTextArea andmed = new JTextArea();
+		andmed.append(" " + kursus + uusRida);
+		andmed.append(" " + nimi + uusRida);
+		andmed.append(" " + juht + uusRida);
+		andmed.append(" " + email + uusRida);
+		andmed.append(" " + liikmed);
+		andmed.setEditable(false);
 		
-		ImageIcon pilt = new ImageIcon("./img/troll.png");
-		JLabel pildiKast = new JLabel(pilt);
-		pildiKast.setVisible(true);
-		raam.add(pildiKast);
+		URL s = IntroUI.class.getResource("");
+		String fail = s + "img/troll.png";
+		try {
+			asukoht = new URL(fail);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		System.out.println(asukoht);
+		ImageIcon pilt = new ImageIcon(asukoht);
+		
+		label1 = new JLabel(pilt, JLabel.CENTER);
+		label2 = andmed;
+		sisu.add(label2);
+		sisu.add(label1);
 		
 		
 		raam.setLocationRelativeTo(null); 
-		raam.pack();
+		//raam.pack();
 		raam.setSize(300, 300);
 		raam.setVisible(true); 
 	}
