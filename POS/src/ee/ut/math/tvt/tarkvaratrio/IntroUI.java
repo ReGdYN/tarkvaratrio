f957a2d40d418c200806e7fafd89ebb67911fb63
regdyn@gmail.com <regdyn@gmail.com@ab323cf8-aadf-9211-8cbf-a8e3bd1d72ed>
2012-10-03 10:00:32 +0000

    Just a commit test

---
+++ POS/src/ee/ut/math/tvt/tarkvaratrio/IntroUI.java
@@ -0,0 +1,11 @@
+package ee.ut.math.tvt.tarkvaratrio;
+
+import java.awt.*;
+import javax.swing.*; 
+
+public class IntroUI {
+	private static void createWindow() {
+		JFrame frame = new JFrame("IntroUI");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
