f403cdc4861cedd64bc495e972ef620ac2dcb651
regdyn@gmail.com <regdyn@gmail.com@ab323cf8-aadf-9211-8cbf-a8e3bd1d72ed>
2012-10-14 05:53:44 +0000

    Peaaegu valmis, log4j külge, peab vaatama, sest praegu main class iseenesest ei tööta, aga kui build.xml jooksutate ja pärast arvuti konsooli: java -jar "C:/.../build/jar/Tarkvaratrio.jar" siis see näitab kõike ilusti

--- trunk/POS/src/ee/ut/math/tvt/tarkvaratrio/IntroUI.java
+++ trunk/POS/src/ee/ut/math/tvt/tarkvaratrio/IntroUI.java
@@ -1,29 +1,53 @@
 package ee.ut.math.tvt.tarkvaratrio;
 
 import java.awt.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Properties;
 
 import javax.swing.*; 
 
-@SuppressWarnings("unused")
 public class IntroUI {
 	
-	static String kursus = "2.kursus";
-	static String nimi = "Tarkvaratrio";
-	static String juht = "Taavi Jaanson";
-	static String email = "regdyn@gmail.com";
-	static String liikmed = "Kristjan-Julius Laak, Henri Molloka";
+	static String kursus;
+	static String nimi;
+	static String juht;
+	static String email;
+	static String liikmed;
+	static String version;
 	private final static String uusRida = "\n";
 	static URL asukoht;
 	
+	
 	static void createWindow() {
+		
 		JFrame raam = new JFrame("IntroUI");
 		raam.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container sisu = raam.getContentPane();
 		sisu.setLayout(new GridLayout(3,1));
 		JLabel label1;
 		JTextArea label2, label3;
+		
+		InputStream app_path = null; 
+		app_path = ClassLoader.getSystemResourceAsStream("application.properties");
+		Properties app_props = new Properties(); 
+		try {
+			app_props.load(app_path);
+			kursus = app_props.getProperty("kursus");
+			nimi = app_props.getProperty("nimi");
+			juht = app_props.getProperty("juht");
+			email = app_props.getProperty("email");
+			liikmed = app_props.getProperty("liikmed");
+			
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
 		
 		JTextArea andmed = new JTextArea();
 		andmed.append(" " + kursus + uusRida);
@@ -40,18 +64,36 @@ public class IntroUI {
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
-		System.out.println(asukoht);
 		ImageIcon pilt = new ImageIcon(asukoht);
+		
+		JTextArea versioon = new JTextArea();
+		versioon.setEditable(false);
+		
+		InputStream ver_path = null; 
+		ver_path = ClassLoader.getSystemResourceAsStream("version.properties");
+		Properties ver_props = new Properties(); 
+		try {
+			ver_props.load(ver_path);
+			version = ver_props.getProperty("build.number");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		versioon.append(version);
 		
 		label1 = new JLabel(pilt, JLabel.CENTER);
 		label2 = andmed;
+		label3 = versioon;
 		sisu.add(label2);
 		sisu.add(label1);
+		sisu.add(label3);
 		
 		
 		raam.setLocationRelativeTo(null); 
 		//raam.pack();
 		raam.setSize(300, 300);
 		raam.setVisible(true); 
+		
 	}
 }
