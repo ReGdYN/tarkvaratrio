c275896be8ac791a32471c547aecbea97079d0ef
henri.molloka@gmail.com <henri.molloka@gmail.com@ab323cf8-aadf-9211-8cbf-a8e3bd1d72ed>
2012-11-13 20:21:19 +0000

    Fixed history tab load.

--- trunk/POS/src/ee/ut/math/tvt/salessystem/ui/model/SalesSystemModel.java
+++ trunk/POS/src/ee/ut/math/tvt/salessystem/ui/model/SalesSystemModel.java
@@ -35,6 +35,9 @@ public class SalesSystemModel {
 
         // populate stock model with data from the warehouse
         warehouseTableModel.populateWithData(domainController.loadWarehouseState());
+        
+        //
+        historyTableModel.populateWithData(domainController.loadHistoryState());
 
     }
 
